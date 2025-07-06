3c615ac1878db5122b8e48a2a5cf213e556cf16b
Sanjana023 <duttasanjana237@gmail.com>
2025-07-06 22:23:40 +0530

    favories bug fixed

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,23 +1,27 @@
 import '../css/Favorites.css';
 import { useMovieContext } from '../contexts/MovieContext';
-import { MovieCard } from '../components/MovieCard';
+import {MovieCard} from '../components/MovieCard';
 
 export function Favorites() {
   const { favorites } = useMovieContext();
+
   if (favorites) {
-    <div className="favorites">
-      <h2>Your Favorites</h2>
-      <div className="movies-grid">
-        {favorites.map((movie) => (
-          <MovieCard movie={movie} key={movie.id} />
-        ))}
+    return (
+      <div className="favorites">
+        <h2>Your Favorites</h2>
+        <div className="movies-grid">
+          {favorites.map((movie) => (
+            <MovieCard movie={movie} key={movie.id} />
+          ))}
+        </div>
       </div>
-    </div>;
+    );
   }
+
   return (
     <div className="favorites-empty">
       <h2>No Favorite Movies Yet</h2>
-      <p>Start adding movies to your favorites and they will appear here</p>
+      <p>Start adding movies to your favorites and they will appear here!</p>
     </div>
   );
 }
