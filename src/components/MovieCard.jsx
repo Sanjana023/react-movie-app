c16cdef0d60deccf72bd8d779254ab0fd4552016
Sanjana023 <duttasanjana237@gmail.com>
2025-07-06 22:08:22 +0530

    final commit

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,14 +1,22 @@
 import '../css/MovieCard.css';
+import { useMovieContext } from '../contexts/MovieContext';
+
 export function MovieCard({ movie }) {
-  function onFavoriteClick() {
-    alert('clicked');
+  const {addToFavorites,removeFromFavorites,isFavorite} = useMovieContext();
+  const favorite = isFavorite(movie.id);
+
+  function onFavoriteClick(e) {
+     e.preventDefault();
+     if(favorite) removeFromFavorites(movie.id);
+     else addToFavorites(movie);
+
   }
   return (
     <div className="movie-card">
       <div className="movie-poster">
         <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
         <div className="movie-overlay">
-          <button className="favorite-btn" onClick={onFavoriteClick}>
+          <button className= {`favorite-btn ${favorite ? "active" : "" }`} onClick={onFavoriteClick}>
             ♥
           </button>
         </div>
--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,8 +1,23 @@
 import '../css/Favorites.css';
-export function Favorites(){
-    return <div className="favorites-empty">
-        <h2>No Favorite Movies Yet</h2>
-        <p>Start adding movies to your favorites and they will appear here</p>
+import { useMovieContext } from '../contexts/MovieContext';
+import { MovieCard } from '../components/MovieCard';
 
+export function Favorites() {
+  const { favorites } = useMovieContext();
+  if (favorites) {
+    <div className="favorites">
+      <h2>Your Favorites</h2>
+      <div className="movies-grid">
+        {favorites.map((movie) => (
+          <MovieCard movie={movie} key={movie.id} />
+        ))}
+      </div>
+    </div>;
+  }
+  return (
+    <div className="favorites-empty">
+      <h2>No Favorite Movies Yet</h2>
+      <p>Start adding movies to your favorites and they will appear here</p>
     </div>
+  );
 }
