f2d8668da5ed8245bd606a985c91acb60f83bc1a
Sanjana023 <duttasanjana237@gmail.com>
2025-07-06 19:03:35 +0530

    NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,22 @@ import './App.css';
 import { MovieCard } from './components/MovieCard';
 import { Favorites } from './pages/Favorites';
 import { Home } from './pages/Home';
-import {ErrorPage} from './pages/Error'
+import { ErrorPage } from './pages/Error';
 import { Routes, Route } from 'react-router-dom';
+import { NavBar } from './pages/NavBar';
 
 function App() {
   return (
-     <main className="main-content">
-      <Routes>
-        <Route path="/" element={<Home />}/>
-        <Route path="/favorites" element={<Favorites />}/>
-        <Route path="*" element={<ErrorPage />} />
-      </Routes>
-     </main>
+    <div>
+      <NavBar />
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favorites" element={<Favorites />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
+      </main>
+    </div>
   );
 }
 
